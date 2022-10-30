36679b84ffdd76c0619f2442eae694e3cb147c17
tylercchase <tylercchase@gmail.com>
2022-10-29 21:04:38 -0400

    Basic grass running

---
+++ src/Grass/Grass.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var grass : GPUParticles3D = $Grass
+	var coll = $Colliision.get_texture()
+
+	grass.process_material.set_shader_parameter('displace', coll)
+
+
