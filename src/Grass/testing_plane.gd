5c0fc15f24167f18142fc943a7da6e71d91a34a6
tylercchase <tylercchase@gmail.com>
2022-12-06 12:09:29 -0500

    Multi object demo

--- src/Grass/testing_plane.gd
+++ src/Grass/testing_plane.gd
@@ -1,4 +1,3 @@
-@tool
 extends MeshInstance3D
 
 
--- src/Singletons/DisplacementBuffer.gd
+++ src/Singletons/DisplacementBuffer.gd
@@ -1,10 +1,9 @@
-@tool
 extends Node
 
 var objects = {
 	
 }
-var player
+var player: CharacterBody3D
 var texture: Image
 var previous_frame
 
@@ -12,15 +11,19 @@ var previous_frame
 var size = 1024
 
 var total_difference = Vector2()
-
+var timer = Timer.new()
 var player_last_pos = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	texture = Image.create(size, size, false, Image.FORMAT_RGBA8)
 	texture.fill(Color(0.5, 0.5, 0.0, 1.0))
+	timer.one_shot = true
+	timer.wait_time = 0.5
+	add_child(timer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+
 	var scale_factor = 20
 	
 	texture.fill(Color(0.5, 0.5, 0.0, 1.0))
@@ -28,7 +31,7 @@ func _process(_delta):
 	var things = []
 	for object in objects.keys():
 		var relative = player.global_position - objects[object].ref.global_position
-		things.push_back({"x": relative.x, "y": relative.z, "r": 3})
+		things.push_back({"x": relative.x, "y": relative.z, "r": objects[object].r * 20})
 	# do cool stuff to make a texture that moves and adds these objects to the game?
 	
 	# move the texture based off of how far the player has moved
@@ -36,18 +39,26 @@ func _process(_delta):
 	if difference != Vector2(0,0):
 		player_last_pos = Vector2(player.global_position.x,player.global_position.z)
 		total_difference += difference
-		if abs(total_difference.x) >= 1.0 || abs(total_difference.y) >= 1.0:
-			print('should change now')
-	
+		if !timer.is_stopped() && abs(total_difference.x) >= 1.0 || abs(total_difference.y) >= 1.0:
+			# shift all the pixel now by however much
+			# if it goes outside the bounds then just insert 0
+			# this is ridiculously expensive...
+			# compute shader???
+			for x in range(0,size):
+				for y in range(0,size):
+					var shifted_coordinates = 1 #Vector2(x + total_difference.x, y + total_difference.y)
+			total_difference = Vector2.ZERO
 	# eventually shift all of the pixel
 	
 	# draw circles, will overwrite any shifts on constant objects
 	for thing in things:
-#		print(thing.x,thing.y,thing.r)
-		draw_circle(thing.x * scale_factor,thing.y * scale_factor,25)
-	draw_circle(0,0,20) # player draw constant for now, should probably check if player is on ground
-
+		draw_circle(thing.x * scale_factor,thing.y * scale_factor,thing.r)
+	if player.is_on_floor():
+		draw_circle(0,0,15)
 	# go through every pixel and conver it to -1 to 1 and then bring towards 0
+	
+	timer.start()
+	
 func draw_circle(_x,_y,radius):
 	# center of screen should be 0,0
 	var point = Vector2(512 + _x, 512 + _y)
@@ -56,7 +67,17 @@ func draw_circle(_x,_y,radius):
 			var distance = Vector2(x,y).distance_to(point) 
 			if distance <= radius:
 				# should draw the angle from the two points into the red and green channel and scale blue value based off of radius and distance from center
-				texture.set_pixel(x,y,Color.BLUE)
+#				texture.set_pixel(x,y,Color.BLUE)
+				var flattening = 0.0
+				var x_rotation = 0.0
+				var z_rotation = 0.0
+		
+				x_rotation = (x - point.x  + 1.0) / 2.0 
+				z_rotation = (y - point.y + 1.0) / 2.0 
+				
+				flattening = 1.0 - distance / radius
+				texture.set_pixel(x,y,Color(x_rotation, z_rotation, flattening))
+				
 
 func add_player(object: Node):
 	player = object
